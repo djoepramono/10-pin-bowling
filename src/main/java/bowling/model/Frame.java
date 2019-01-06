dd1829103b071bb3236b9c0ab40744ee921721f0
Djoe Pramono <djoepramono@gmail.com>
2019-01-07 02:12:19 +1100

    add unused is valid next bowl

--- src/main/java/bowling/model/Frame.java
+++ src/main/java/bowling/model/Frame.java
@@ -57,4 +57,66 @@ public class Frame {
         System.out.println("  calculated " + bowlDisplay + " as " + bowlScore);
         return bowlScore;
     }
+
+    // Function to check if the display score is valid given the existing display score in a frame
+    // e.g. 2/ is valid, but 2X is not valid
+    // Atm ignore all numbers, as we check it in other function, but we might need to revisit
+    // Thankfully we have a rule where there's maximum 2 bowls per frame, so validation is a little easier
+    public static Boolean isValidNextBowl(Frame frame, String bowlDisplay) {
+        Boolean isValid = false;
+        // try {
+        //     bowlScore = Integer.parseInt(bowlDisplay);
+        // } catch(NumberFormatException e) {
+        //     switch(bowlDisplay) {
+        //         case "-": isValid = true; break;
+        //         case "/":
+        //             //if there's already a integer in the frame, then it's valid, otherwise invalid
+        //             if Frame.bowls.stream().map(b -> parseInt(b.knockedPins));
+        //             break;
+        //         case "X": bowlScore = frameMaxPins - frameKnockedPins; break;
+        //         default: bowlScore = 0;
+        //     }
+        // }
+
+        if (isNumeric(bowlDisplay)) {
+            //ignore for now
+            isValid = true;
+        } else {
+            switch(bowlDisplay) {
+                case "-":
+                    // always valid
+                    isValid = true;
+                    break;
+                case "/":
+                    // only valid when:
+                    // - if there's already a integer in the frame
+                    isValid = false;
+                    isValid = frame.bowls.stream()
+                        .map(b -> isNumeric(b.display))
+                        .reduce(false, (a, b) -> a ); // check again
+                    // - if there's already a `-`
+                    break;
+                case "X":
+                    // valid if the frame is empty
+                    isValid = frame.bowls.isEmpty();
+                    break;
+                default:
+                    isValid = false;
+            }
+        }
+
+        return isValid;
+    }
+
+    // I can't believe this "PURE" function is not in a standard library ...
+    private static Boolean isNumeric(String str)
+    {
+        try {
+            double d = Double.parseDouble(str);
+        }
+        catch(NumberFormatException nfe){
+            return false;
+        }
+        return true;
+    }
 }
