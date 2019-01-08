d3550ac038ce1073c16d99732b5ff1b69f838583
Djoe Pramono <djoepramono@gmail.com>
2019-01-08 16:30:31 +1100

    0 is not valid

--- src/main/java/bowling/App.java
+++ src/main/java/bowling/App.java
@@ -12,15 +12,20 @@ public class App {
 
         Game game = new Game();
 
-        String[] entries = {"1","3","X","9","/","4","2"};
+        // String[] entries = {"1","3","X","9","/","4","2"};
         // String[] entries = {"1","s","X","9","/","4"};
-        // String[] entries = {"1","3","X","9","5","4"};
+//         String[] entries = {"-","/","1","2","X","9","5","4"};
+//        String[] entries = {"-","/","1","2","X","2","5","4"}; // correct 42 not 38
+        String[] entries = {"-","/","1","2","X","2"}; // correct 28 not 24
+//        String[] entries = {"-","/","1","2","X","2","5"}; // correct 38 not 24
+
+//        String[] entries = {"X","/","X","9","/","4"};
 
         try {
             for(int i=0; i < entries.length; i++){
                 if (validateStringEntry(entries[i])) {
                     game.process(entries[i]);
-                    System.out.println("The total score is " + game.getTotalScore());
+                    System.out.println("The total score is " + game.getTotalScore(game.getAllBowls(game.frames)));
                 } else
                 {
                     System.out.println("The entry " + entries[i] + " is not a valid string");
@@ -35,6 +40,6 @@ public class App {
 
     private static Boolean validateStringEntry(String entry) {
         // this is a string match you don't need `/ /g`
-        return entry.matches("^[0-9|X|\\/|-]$");
+        return entry.matches("^[1-9|X|\\/|-]$");
     }
 }
