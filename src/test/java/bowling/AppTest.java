3e5160167180482508dd0bef88f6cab94e6643b4
Djoe Pramono <djoepramono@gmail.com>
2019-01-16 23:55:25 +1100

    call App.process in static way

--- src/test/java/bowling/AppTest.java
+++ src/test/java/bowling/AppTest.java
@@ -10,68 +10,48 @@ class AppTest {
 
     @Test
     public void processSuccessfulEntries() throws BowlingException {
-
         String[] entries = {"-","/","1","2","X","2"};
-        App app = new App();
-        Integer score = app.process(entries);
-
+        Integer score = App.process(entries);
         assertEquals((Integer) 28, score);
     }
 
     @Test
     public void processInvalidEntries() {
-
         String[] entries = {"A","1","2","3"};
-        App app = new App();
-
-        assertThrows(BowlingException.class, () -> app.process(entries));
+        assertThrows(BowlingException.class, () -> App.process(entries));
     }
 
     @Test
     public void processInvalidSequence() {
-
         String[] entries = {"/","1","2","3"};
-        App app = new App();
-
-        assertThrows(BowlingException.class, () -> app.process(entries));
+        assertThrows(BowlingException.class, () -> App.process(entries));
     }
 
     @Test
     public void processInvalidNumericZero() {
-
         String[] entries = {"0","1","2","3"};
-        App app = new App();
-
-        assertThrows(BowlingException.class, () -> app.process(entries));
+        assertThrows(BowlingException.class, () -> App.process(entries));
     }
 
     @Test
     public void processFirstSampleEntriesTest() throws BowlingException {
-
         String[] entries = {"X","X","X","X","X","X","X","X","X","X","X","X"};
-        App app = new App();
-        Integer score = app.process(entries);
+        Integer score = App.process(entries);
 
         assertEquals((Integer) 300, score);
     }
 
     @Test
     public void processSecondSampleEntriesTest() throws BowlingException {
-
         String[] entries = {"9-","9-","9-","9-","9-","9-","9-","9-","9-","9-"};
-        App app = new App();
-        Integer score = app.process(entries);
-
+        Integer score = App.process(entries);
         assertEquals((Integer) 90, score);
     }
 
     @Test
     public void processThirdSampleEntriesTest() throws BowlingException {
-
         String[] entries = {"5/","5/","5/","5/","5/","5/","5/","5/","5/","5/", "5"};
-        App app = new App();
-        Integer score = app.process(entries);
-
+        Integer score = App.process(entries);
         assertEquals((Integer) 150, score);
     }
 
